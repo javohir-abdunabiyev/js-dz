681b3a2a38b4f363af96a9c702a7376d06a67612
javohir-abdunabiyev <jahaajahaaa2@gmail.com>
2024-06-21 14:32:38 +0500

    added backend

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+fetch('http://localhost:8080/todos')
+    .then(res => res.json())
+    .then(data => {
+        todos.push(...data.todos);
+        reload(todos, cont);
+    });
 const form = document.forms.namedItem("todo")
 
 const todos = []
@@ -13,26 +19,38 @@ form.onsubmit = (event) => {
         time: new Date(Date.now()).toISOString(),
         isDone: false
     }
-    todos.push(task)
-    reload(todos, cont)
-    console.log(task);
+
+
+
+    fetch('http://localhost:8080/todos', {
+        method: 'post',
+        body: JSON.stringify(task)
+    })
+    .then(res => res.json())
+    .then(Task => {
+        todos.push(Task);
+        reload(todos, cont);
+    });
+
     form.reset()
-    
+
 }
 
 function reload(arr, place) {
     place.innerHTML = ""
-    
-    for(let item of arr) {
+
+    for (let item of arr) {
         const items = document.createElement("div")
         items.classList.add("item")
-        
+
         const top_side = document.createElement("div")
         top_side.classList.add("top-side")
         const span = document.createElement("span")
         span.textContent = item.title
 
-        if(item.isDone === true) {
+
+
+        if (item.isDone === true) {
             span.classList.add("line")
         }
         span.onclick = () => {
@@ -43,40 +61,48 @@ function reload(arr, place) {
 
         items.ondblclick = () => {
             let edit = prompt("Write")
-            if(edit) {
+            if (edit) {
                 item.title = edit;
                 span.innerHTML = edit
             }
+
+            fetch(`http://localhost:8080/todos/${item.id}`, {
+                method: 'put',
+                body: JSON.stringify({ title: edit })
+            })
+            .then(res => res.json())
+
         }
 
 
         const btn = document.createElement("button")
         btn.innerHTML = "x"
-        
+
         const time_span = document.createElement("span")
         time_span.innerHTML = item.time
         time_span.classList.add("time")
-        
-        
+
+
 
 
         btn.onclick = () => {
-            let close = Boolean(todos.find(el => el.id === item.id))
 
-            if(close !== -1) {
-                let idx = todos.indexOf(item)
-                todos.splice(idx, 1)
-                reload(todos, cont)
-            }
+            fetch(`http://localhost:8080/todos/${item.id}`, {
+                method: 'delete'
+            })
+            .then((todos) => {
+                todos = todos.filter(todo => todo.id !== item.id);
+                reload(todos, cont);
+            })
+            
         }
-        
+
         top_side.append(span, btn)
-        
+
         items.append(top_side, time_span)
-        
+
         cont.append(items)
     }
 
 }
 
-console.log(todos);
